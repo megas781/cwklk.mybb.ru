b3a2dabddad156a0d8c037b34afa879510e1cdf5
glebkalachev <megas781@gmail.com>
2019-07-19 13:37:06 +0400

    implemented basic menu reveal

--- src/entry.js
+++ src/entry.js
@@ -1,2 +1,15 @@
 require('./style/compose.scss');
 require('./style_cs/compose_cs.scss');
+
+document.querySelectorAll('#pun-announcement tr:nth-child(1) td:nth-child(1), #pun-announcement tr:nth-child(1) td:nth-child(3)').forEach(function (cell) {
+    cell.addEventListener('click', function (e) {
+        console.log('click!');
+        this.querySelectorAll('a').forEach(function (link) {
+            if (link.style.display === 'block') {
+                link.style.display = 'none';
+            } else {
+                link.style.display = 'block';
+            }
+        })
+    })
+});
